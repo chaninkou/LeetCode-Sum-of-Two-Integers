653ad592c193ac4ce25cb2b4ea3ce2d7ea833aa5
chaninkou <michaelkoulove@yahoo.com>
2019-12-19 23:58:56 -0800

    Great review on bitwise, adding without plus sign using &,^,<<.

--- src/adding/AddTwoIntegerWithoutSymbolFunction.java
+++ src/adding/AddTwoIntegerWithoutSymbolFunction.java
@@ -2,26 +2,28 @@ package adding;
 
 public class AddTwoIntegerWithoutSymbolFunction {
 	public int getSum(int a, int b) {
-		if (a == 0) {
+		// Easy check to see if one of them is 0
+		if(a == 0){
 			return b;
 		}
-		if (b == 0) {
+		if(b == 0){
 			return a;
 		}
-
-		// Until we have no more carry
-		while (b != 0) {
-			// & is only 1 when both are 1, to see the position we need carry
+		
+		// b will be 0 when there are no more carry
+		while(b != 0){
+			// & will get the position where the carry are, 1 & 1 is 1, when you add two binary, there is a carry
 			int carry = a & b;
-
-			// XOR with can't be 00 or 11, simulating addition and store it in a
+			
+			// A will be the running addition result
+			// XOR will act as a normal adding, cause only 0 ^ 1 or 1 ^ 0 is 1
 			a = a ^ b;
-
-			// b hold carry, Make carry move to the left for the next iteration
-			// everytime
+			
+			// b is the carries
+			// Need to left shift one because we don't want the raw carry position
 			b = carry << 1;
 		}
-
+		
 		return a;
 	}
 }
